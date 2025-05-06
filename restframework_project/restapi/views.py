49622f82a8009e557f4573359864f62b7bbfd170
unknown <atolagbefolakerach@gmail.com>
2025-05-06 09:30:02 +0100

    first commit

---
+++ restframework_project/restapi/views.py
@@ -0,0 +1,15 @@
+from django.shortcuts import render
+from rest_framework.request import Request
+from rest_framework.response import Response
+from rest_framework import status
+from rest_framework.decorators import api_view
+
+@api_view(http_method_names=["GET","POST"])
+def homepage(request:Request):
+
+    if request.method == "POST":
+        response={"message":"successful created"}
+        return Response(data=response, status=status.HTTP_201_CREATED)
+    
+    response = {"message":"Hello World"}
+    return Response(data=response, status=status.HTTP_200_OK)
