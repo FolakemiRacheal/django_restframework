468890b6c982c27860fc5c02aa9d750336e3f1b5
unknown <atolagbefolakerach@gmail.com>
2025-05-06 12:15:17 +0100

    updated

--- restframework_project/restapi/urls.py
+++ restframework_project/restapi/urls.py
@@ -2,5 +2,7 @@ from .import views
 from django.urls import path
 
 urlpatterns = [
-   path("homepage/", views.homepage, name = "homepage") 
+   path("homepage/", views.homepage, name = "homepage"),
+   path("getall/",views.list_posts, name="list_post"),
+   path("<int:post_index>", views.post_detail,name = "post_details")
 ]
--- restframework_project/restapi/views.py
+++ restframework_project/restapi/views.py
@@ -4,6 +4,26 @@ from rest_framework.response import Response
 from rest_framework import status
 from rest_framework.decorators import api_view
 
+
+posts =[
+
+    {
+        "id":1,
+        "title":"why is it difficult to learn Programming ?",
+        "content":"This is to give reasons why it is hard"
+    },
+     {
+        "id":2,
+        "title":"Tell me all about Javescript ?",
+        "content":"will you prepare python to javascript"
+    },
+     {
+        "id":3,
+        "title":"All programming languages are easy to understand ?",
+        "content":"This is to give reasons why it is hard"
+    }
+]
+
 @api_view(http_method_names=["GET","POST"])
 def homepage(request:Request):
 
@@ -13,3 +33,17 @@ def homepage(request:Request):
     
     response = {"message":"Hello World"}
     return Response(data=response, status=status.HTTP_200_OK)
+
+
+@api_view(http_method_names=["GET"])
+def list_posts(request:Request):
+    return Response(data = posts,status=status.HTTP_200_OK)
+
+
+@api_view(http_method_names=["GET"])
+def post_detail(request:Request, post_index:int):
+    post = posts[post_index]
+
+    if post:
+        return Response(data=post,status=status.HTTP_200_OK)
+    return Response(data={error:"Post not found"},status=status.HTTP_404_NOT_FOUND)
